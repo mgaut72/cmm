4ef82c5708e12d77f4ea3cdfac4944f7a568466a
Matt Gautreau <mgaut72@gmail.com>
2014-09-15 21:30:06 -0700

    add more statement typechecking

--- tests/TypeCheckStatement.hs
+++ tests/TypeCheckStatement.hs
@@ -22,7 +22,7 @@ initialState = Tables { _symbols = M.fromList symbolList
 
 symbolList = [ ("a", TInt), ("b", TChar), ("none", TInt)
              , ("one", TInt), ("two", TInt), ("aa", TArray TInt)
-             , ("bb", TArray TChar)
+             , ("bb", TArray TChar), ("i", TInt)
              ]
 fcnList = [("none", []), ("one", [TInt]), ("two", [TInt, TChar])]
 
@@ -44,7 +44,8 @@ instance (Eq ParseError) where
 
 
 tests = test
-  [ "if" ~: good $ If (Relative Leq (LitChar '1') (LitInt 2)) None
+  [ "none" ~: good $ None
+  , "if" ~: good $ If (Relative Leq (LitChar '1') (LitInt 2)) None
   , "if" ~: good $ If (Logical And (Not (Relative Leq (LitChar '1') (LitInt 2))) (Not (Relative Leq (LitChar '1') (LitInt 2)))) None
   , "if" ~: bad  $ If (Binary Plus (LitChar '1') (LitInt 2)) None
   , "ife" ~: good $ IfElse (Relative Leq (LitChar '1') (LitInt 2)) None None
@@ -55,6 +56,48 @@ tests = test
   , "while" ~: bad  $ While (Binary Plus (LitChar '1') (LitInt 2)) None
   , "return" ~: good $ Return Nothing
   , "return" ~: good $ Return (Just (LitInt 1))
-  , "none" ~: good $ None
   , "for" ~: good $ For Nothing Nothing Nothing None
+  , "for" ~: good $ For Nothing
+                        (Just (Relative Leq (LitChar '1') (LitInt 2)))
+                        Nothing
+                        None
+  , "for" ~: good $ For (Just (Assignment (Scalar "i") (LitInt 0)))
+                        (Just (Relative Leq (Var (Scalar "i")) (LitInt 5)))
+                        Nothing
+                        None
+  , "for" ~: good $ For
+                    (Just (Assignment (Scalar "i") (LitInt 0)))
+                    (Just (Relative Leq (Var (Scalar "i")) (LitInt 5)))
+                    (Just (Assignment (Scalar "i") (Binary Plus (Var (Scalar "i")) (LitInt 1))))
+                    None
+  , "for" ~: bad $  For
+                    (Just (Assignment (Scalar "j") (LitInt 0))) -- j does not exist
+                    (Just (Relative Leq (Var (Scalar "i")) (LitInt 5)))
+                    (Just (Assignment (Scalar "i") (Binary Plus (Var (Scalar "i")) (LitInt 1))))
+                    None
+  , "for" ~: bad $  For
+                    (Just (Assignment (Scalar "i") (LitInt 0)))
+                    (Just (Relative Leq (Var (Scalar "j")) (LitInt 5))) -- j does not exist
+                    (Just (Assignment (Scalar "i") (Binary Plus (Var (Scalar "i")) (LitInt 1))))
+                    None
+  , "for" ~: bad $  For
+                    (Just (Assignment (Scalar "i") (LitInt 0)))
+                    (Just (Relative Leq (Var (Scalar "j")) (LitInt 5)))
+                    (Just (Assignment (Scalar "i") (Binary Plus (Var (Scalar "j")) (LitInt 1)))) -- j does not exist
+                    None
+  , "for" ~: bad $  For
+                    (Just (Assignment (Scalar "i") (LitInt 0)))
+                    (Just (Relative Leq (Var (Scalar "j")) (LitInt 5)))
+                    (Just (Assignment (Scalar "j") (Binary Plus (Var (Scalar "i")) (LitInt 1)))) -- j does not exist
+                    None
+  , "bracketed" ~: good $ Bracketed [ None ]
+  , "bracketed" ~: good $ Bracketed [ None, None ]
+  , "bracketed" ~: bad  $ Bracketed [ None
+                                    , While (Binary Plus (LitChar '1') (LitInt 2)) None
+                                    , None
+                                    ]
+  , "bracketed" ~: good $ Bracketed [ None
+                                    , If (Relative Leq (LitChar '1') (LitInt 2)) None
+                                    , None
+                                    ]
   ]
