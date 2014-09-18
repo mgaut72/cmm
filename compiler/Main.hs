920e0ca825ae51bf80a7c08f18612775e0fa19e4
Matt Gautreau <mgaut72@gmail.com>
2014-09-18 15:53:52 -0700

    add show instances that could again be re-compiled

--- compiler/Main.hs
+++ compiler/Main.hs
@@ -5,5 +5,5 @@ import Language.CMM.Compiler
 main = do
   program <- getContents           -- read from stdin
   case compileCMM program of
-       Right a -> putStrLn $ ("matched : " ++ show a)
+       Right a -> putStrLn $ show a
        Left  a -> putStrLn $ show a
