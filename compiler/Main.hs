a86023e142ccd1fcc6d31092d1742e7aaeb1797f
Matt Gautreau <mgaut72@gmail.com>
2014-09-21 12:33:24 -0700

    print to stderr for parse errors

--- compiler/Main.hs
+++ compiler/Main.hs
@@ -1,11 +1,11 @@
 module Main where
-
+import System.IO
 import Language.CMM.Compiler
 
 main = do
   program <- getContents           -- read from stdin
   let (res, errors) = compileCMM program
-  putStr $ unlines errors
+  hPutStr stderr $ unlines errors
   case res of
        Right a -> putStrLn $ show a
-       Left  a -> putStrLn $ show a
+       Left  a -> hPutStrLn $ show a
