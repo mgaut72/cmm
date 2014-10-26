dd70c28891f875321f6ba879d00733090ea2d91e
Matt Gautreau <mgaut72@gmail.com>
2014-10-26 00:11:57 -0700

    revert compiler printing behavior

--- compiler/Main.hs
+++ compiler/Main.hs
@@ -1,10 +1,14 @@
 module Main where
+
 import System.IO
+import Data.List
+
 import Language.CMM.Compiler
 
 main = do
   program <- getContents
   case compileCMM program of
-       (Right a, []) -> putStrLn $ show a
-       (Right a, es) -> hPutStr stderr $ unlines es
-       (Left  a, es) -> hPutStrLn stderr $ unlines es ++ show a
+       (Right a, []) -> return ()
+       (Right a, es) -> hPutStr stderr $ errorMessages es
+       (Left  a, es) -> hPutStrLn stderr $ errorMessages es ++ show a
+ where errorMessages = unlines . nub
