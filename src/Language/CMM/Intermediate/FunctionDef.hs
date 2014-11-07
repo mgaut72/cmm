0a5551f69518488dcb3ef8efd0eea4556fc0cbda
Matt Gautreau <mgaut72@gmail.com>
2014-11-07 15:54:34 -0700

    possibly incomplete function codegen

--- src/Language/CMM/Intermediate/FunctionDef.hs
+++ src/Language/CMM/Intermediate/FunctionDef.hs
@@ -1,3 +1,13 @@
 module Language.CMM.Intermediate.FunctionDef where
 
+import Control.Applicative
+import Data.Foldable (foldMap)
+import Data.Monoid
+
 import Language.CMM.AST
+import Language.CMM.Intermediate.Instructions
+
+genF :: FunctionDef -> TACGen [ThreeAddress]
+genF (FunctionDef t i ps vs ss) = do
+  body <- foldMap genS ss
+  return $ pure (Call i) <> body
